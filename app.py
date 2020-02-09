23553301b91421fc7dba03431d0daa2ea9f05490
harsohailbrar <harsohail.brar1@gmail.com>
2020-02-09 15:14:22 -0700

    added null field checking

--- app.py
+++ app.py
@@ -20,12 +20,18 @@ def index():
     if request.method == 'POST':
         city = request.form['city']
         country = request.form['country']
+
+        if city is '' or country is '':
+            city = 'london'
+            country = 'GB'
+
         return redirect(url_for('result', city = city, country = country))
     else:
         return render_template('index.html')
 
 @app.route('/result/<city>/<country>')
 def result(city, country):
+    print(city)
     api_link = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," +  country + "&appid=" + API_KEY       
     json = requests.get(api_link).json()
     weather = createWeather(json)
